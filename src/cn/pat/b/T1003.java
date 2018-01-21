75a4cf876ed164b94c50695156a17fe4c6fdb1be
cier <cier@192.168.164.1>
2018-01-21 23:07:50 +0800

    删掉了第三题的代码注释，pat中完美运行

--- src/cn/pat/b/T1003.java
+++ src/cn/pat/b/T1003.java
@@ -5,12 +5,6 @@ import java.util.Scanner;
 public class T1003 {
 
 	public static void main(String[] args) {
-		/*
-		 * Scanner input = new Scanner(System.in); int n = input.nextInt(); String[] s =
-		 * new String[n]; for(int i = 0;i<n;i++) { s[i] = input.next(); } for(int i =
-		 * 0;i<n;i++) { if(isTrue(s[i])) { System.out.println("YES"); }else {
-		 * System.out.println("NO"); } } input.close();
-		 */
 		Scanner in = new Scanner(System.in);
 		int n = in.nextInt();
 		in.nextLine();
@@ -37,9 +31,9 @@ public class T1003 {
 		in.close();
 	}
 
-	public static boolean isTrue(String str) {
-		int p = str.indexOf('P');
-		int t = str.indexOf('T');
+	public static boolean isTrue(String s) {
+		int p = s.indexOf('P');
+		int t = s.indexOf('T');
 		if (p > t) {
 			return false;
 		}
@@ -48,18 +42,17 @@ public class T1003 {
 		String b = null;
 		String c = null;
 		if (p != -1) {
-			a = str.substring(0, p);
+			a = s.substring(0, p);
 		} else {
 			return false;
 		}
 		if (t != -1) {
-			c = str.substring(t + 1, str.length());
+			c = s.substring(t + 1, s.length());
 		} else {
 			return false;
 		}
-		b = str.substring(p + 1, t);
-
-		if (a.contains("P") || a.contains("T") || b.contains("P") || b.contains("T") || c.contains("P")
+		b = s.substring(p + 1, t);
+		if (a.contains("P") || a.contains("T") || b.contains("T") || b.contains("P") || c.contains("P")
 				|| c.contains("T")) {
 			return false;
 		}
@@ -83,47 +76,5 @@ public class T1003 {
 		} else {
 			return false;
 		}
-		// // 判断是否包含PAT以及是否包含其他字符
-		// if(!str.contains("P")||!str.contains("A")||!str.contains("T")) {
-		// return false;
-		// }
-		// if(str.contains("P")&&str.contains("A")&&str.contains("T")) {
-		// // 判断P和T的位置 indexOf判断的是第一个字符的位置
-		// int pIndex = str.indexOf('P');
-		// int tIndex = str.indexOf('T');
-		// if (tIndex < pIndex) {
-		// return false;
-		// }
-		// // 判断P和T的个数
-		// int pCount = 0;
-		// int tCount = 0;
-		// for(int i = 0;i<str.length();i++) {
-		// switch(str.charAt(i)) {
-		// case 'P':
-		// pCount++;
-		// break;
-		// case 'T':
-		// tCount++;
-		// break;
-		// case 'A':
-		// break;
-		// default:
-		// return false;
-		// }
-		// }
-		// if(pCount>1||tCount>1) {
-		// return false;
-		// }
-		// // 找到PT之间A的个数
-		// int aMiddleCount = tIndex - pIndex - 1;
-		// // 找到P之前的A的个数
-		// int aBeforeCount = pIndex;
-		// // 找到T之后的A的个数
-		// int aAfterCount = str.length()-tIndex-1;
-		// if(aBeforeCount*aMiddleCount!=aAfterCount) {
-		// return false;
-		// }
-		// }
-		// return true;
 	}
 }
